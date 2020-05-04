632992c9402a626ba5adb4a5b37c47dfe782ecfd
caio_ingber <caioingber@gmail.com>
2020-05-04 17:36:17 -0500

    added views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from models import Eco, Country
+from serializers import EcoSerializer, CountrySerializer
+from rest_framework import generics
 
-# Create your views here.
+class CountryList(generics.ListAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+
+class EcoList(generics.ListAPIView):
+    queryset = Eco.objects.all()
+    serializer_class = EcoSerializer
+

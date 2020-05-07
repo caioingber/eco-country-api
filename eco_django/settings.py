b7d3cc3829f99c1c02313b25140cad6268ac38cc
caio_ingber <caioingber@gmail.com>
2020-05-07 11:56:42 -0500

    installed dj-database for deployment

--- eco_django/settings.py
+++ eco_django/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'corsheaders',
-    'django_filters'
+    'django_filters',
+    'dj_database_url'
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
